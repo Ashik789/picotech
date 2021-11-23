3e90a63ac4328138212b86ec1ed2f50fd9e455a2
idc.ashik <idc.ashik@gmail.com>
2021-11-23 23:45:57 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,279 @@
+jQuery(window).on("load", function () {
+    "use strict";
+
+    /*  ===================================
+     Loading Timeout
+     ====================================== */
+    $("#loader-fade").fadeOut(800);
+});
+
+jQuery(function ($) {
+    "use strict";
+
+    var $window = $(window);
+    var windowsize = $(window).width();
+
+    /* ===================================
+       Nav Scroll
+       ====================================== */
+
+    $(".scroll").on("click", function(event){
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top - 40}, 1100);
+    });
+    /* ====================================
+       Nav Fixed On Scroll
+       ======================================= */
+
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() >= 80) { // Set position from top to add class
+            $('header').addClass('header-appear');
+        }
+        else {
+            $('header').removeClass('header-appear');
+        }
+    });
+    /*bottom menu fix*/
+    if ($("nav.navbar").hasClass("bottom-nav")) {
+        var navHeight = $(".bottom-nav").offset().top;
+        $(window).on("scroll", function () {
+            if ($window.scrollTop() > navHeight) {
+                $('header').addClass('header-appear');
+            } else {
+                $('header').removeClass('header-appear');
+            }
+        });
+    }
+
+    /* ===================================
+       Side Menu
+       ====================================== */
+
+    if ($(".sidemenu_toggle").length) {
+        $(".sidemenu_toggle").on("click", function () {
+            $(".pushwrap").toggleClass("active");
+            $(".side-menu").addClass("side-menu-active"), $("#close_side_menu").fadeIn(700)
+        }), $("#close_side_menu").on("click", function () {
+            $(".side-menu").removeClass("side-menu-active"), $(this).fadeOut(200), $(".pushwrap").removeClass("active")
+        }), $(".side-nav .navbar-nav .nav-link").on("click", function () {
+            $(".side-menu").removeClass("side-menu-active"), $("#close_side_menu").fadeOut(200), $(".pushwrap").removeClass("active")
+        }), $("#btn_sideNavClose").on("click", function () {
+            $(".side-menu").removeClass("side-menu-active"), $("#close_side_menu").fadeOut(200), $(".pushwrap").removeClass("active")
+        })
+    }
+    /* =====================================
+       Wow
+       ======================================== */
+
+    if ($(window).width() > 767) {
+        var wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 0,
+            mobile: false,
+            live: true
+        });
+        new WOW().init();
+    }
+
+    /* ===================================
+        Animated Cursor
+       ====================================== */
+
+    function animatedCursor() {
+
+        if ($("#animated-cursor").length) {
+
+            var e = {x: 0, y: 0}, t = {x: 0, y: 0}, n = .25, o = !1, a =    document.getElementById("cursor"),
+                i = document.getElementById("cursor-loader");
+            TweenLite.set(a, {xPercent: -50, yPercent: -50}), document.addEventListener("mousemove", function (t) {
+                var n = window.pageYOffset || document.documentElement.scrollTop;
+                e.x = t.pageX, e.y = t.pageY - n
+            }), TweenLite.ticker.addEventListener("tick", function () {
+                o || (t.x += (e.x - t.x) * n, t.y += (e.y - t.y) * n, TweenLite.set(a, {x: t.x, y: t.y}))
+            }),
+                $(".animated-wrap").mouseenter(function (e) {
+                    TweenMax.to(this, .3, {scale: 2}), TweenMax.to(a, .3, {
+                        scale: 2,
+                        borderWidth: "1px",
+                        opacity: .2
+                    }), TweenMax.to(i, .3, {
+                        scale: 2,
+                        borderWidth: "1px",
+                        top: 1,
+                        left: 1
+                    }), TweenMax.to($(this).children(), .3, {scale: .5}), o = !0
+                }),
+                $(".animated-wrap").mouseleave(function (e) {
+                    TweenMax.to(this, .3, {scale: 1}), TweenMax.to(a, .3, {
+                        scale: 1,
+                        borderWidth: "2px",
+                        opacity: 1
+                    }), TweenMax.to(i, .3, {
+                        scale: 1,
+                        borderWidth: "2px",
+                        top: 0,
+                        left: 0
+                    }), TweenMax.to($(this).children(), .3, {scale: 1, x: 0, y: 0}), o = !1
+                }),
+
+                $(".testimonial-images .animated-wrap").mouseenter(function (e) {
+                    TweenMax.to(this, .3, {scale: 2}), TweenMax.to(a, .3, {
+                        scale: 2,
+                        borderWidth: "1px",
+                        opacity: .2
+                    }), TweenMax.to(i, .3, {
+                        scale: 2,
+                        borderWidth: "1px",
+                        top: 1,
+                        left: 1
+                    }), TweenMax.to($(this).children(), .3, {scale: .5}), o = !0
+                }),
+
+                $(".animated-wrap").mousemove(function (e) {
+                    var n, o, i, l, r, d, c, s, p, h, x, u, w, f, m;
+                    n = e, o = 2, i = this.getBoundingClientRect(), l = n.pageX - i.left, r = n.pageY - i.top, d = window.pageYOffset || document.documentElement.scrollTop, t.x = i.left + i.width / 2 + (l - i.width / 2) / o, t.y = i.top + i.height / 2 + (r - i.height / 2 - d) / o, TweenMax.to(a, .3, {
+                        x: t.x,
+                        y: t.y
+                    }), s = e, p = c = this, h = c.querySelector(".animated-element"), x = 20, u = p.getBoundingClientRect(), w = s.pageX - u.left, f = s.pageY - u.top, m = window.pageYOffset || document.documentElement.scrollTop, TweenMax.to(h, .3, {
+                        x: (w - u.width / 2) / u.width * x,
+                        y: (f - u.height / 2 - m) / u.height * x,
+                        ease: Power2.easeOut
+                    })
+                }),
+                $(".hide-cursor,.btn,.tp-bullets").mouseenter(function (e) {
+                    TweenMax.to("#cursor", .2, {borderWidth: "1px", scale: 2, opacity: 0})
+                }), $(".hide-cursor,.btn,.tp-bullets").mouseleave(function (e) {
+                TweenMax.to("#cursor", .3, {borderWidth: "2px", scale: 1, opacity: 1})
+            }),$(".link").mouseenter(function (e) {
+                TweenMax.to("#cursor", .2, {
+                    borderWidth: "0px",
+                    scale: 3,
+                    backgroundColor: "rgba(255,255,255,0.47)",
+                    opacity: .15
+                })
+            }), $(".link").mouseleave(function (e) {
+                TweenMax.to("#cursor", .3, {
+                    borderWidth: "2px",
+                    scale: 1,
+                    backgroundColor: "rgba(255, 255, 255, 0)",
+                    opacity: 1
+                })
+            })
+
+        }
+
+    }
+
+    if ($(window).width() > 991) {
+        setTimeout(function () {
+            animatedCursor();
+        }, 1000);
+    }
+   
+
+
+    /* ===================================
+       Owl Carousel
+       ====================================== */
+
+    /* Team Classic */
+    $(".team-classic.owl-team").owlCarousel({
+        items: 3,
+        margin: 30,
+        dots: true,
+        nav: false,
+        loop:true,
+        autoplay: true,
+        smartSpeed:500,
+        navSpeed: true,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive: {
+            992: {
+                items: 3
+            },
+            600: {
+                items: 2
+            },
+            320: {
+                items: 1
+            }
+        }
+    });
+
+     /* ===================================
+       Revolution Slider
+       ====================================== */
+
+    /*Standard*/
+    $("#secondary-banner").show().revolution({
+        sliderType: "standard",
+        sliderLayout: "fullscreen",
+        scrollbarDrag: "true",
+        dottedOverlay: "none",
+        navigation: {
+            keyboardNavigation: "off",
+            keyboard_direction: "horizontal",
+            mouseScrollNavigation: "off",
+            mouseScrollReverse: "default",
+            onHoverStop: "on",
+            touch: {
+                touchenabled: "on",
+                swipe_threshold: 75,
+                swipe_min_touches: 1,
+                swipe_direction: "horizontal",
+                drag_block_vertical: false
+            },
+            bullets: {
+                enable: true,
+                hide_onmobile: false,
+                style: "awaza",
+                hide_onleave: false,
+                direction: "vertical",
+                h_align: "left",
+                v_align: "center",
+                h_offset: 50,
+                v_offset: 0,
+                space: 5,
+                tmp: '<div class="tp-bullet-number"><span class="tp-count">{{param1}}</span><span class="tp-bullet-line"></span></div>'
+            }
+        },
+        viewPort: {
+            enable: true,
+            outof: "pause",
+            visible_area: "90%"
+        },
+        responsiveLevels: [1240, 1024, 778, 480],
+        visibilityLevels: [1240, 1024, 778, 480],
+        gridwidth: [1240, 1024, 778, 480],
+        gridheight: [868, 768, 960, 720],
+        lazyType: "none",
+        parallax: {
+            type: "mouse",
+            origo: "slidercenter",
+            speed: 9000,
+            levels: [2, 3, 4, 5, 6, 7, 12, 16, 10, 50],
+            disable_onmobile: "on"
+        },
+        shadow: 0,
+        spinner: "off",
+        stopLoop: "off",
+        stopAfterLoops: -1,
+        stopAtSlide: -1,
+        shuffle: "off",
+        autoHeight: "off",
+        hideThumbsOnMobile: "off",
+        hideSliderAtLimit: 0,
+        hideCaptionAtLimit: 0,
+        hideAllCaptionAtLilmit: 0,
+        debugMode: false,
+        fallbacks: {
+            simplifyAll: "off",
+            nextSlideOnWindowFocus: "off",
+            disableFocusListener: false,
+        }
+    });
+
+});
